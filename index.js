7b5f078808926c89bf80896cd7b7bcaff47be049
aswoona <idkcash01@gmail.com>
2026-04-08 19:47:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,27 @@ function onHeaderClickOutside(e) {
 }
 
 
+function toggleHeader() {
+    if (isHeaderCollapsed) {
+        // collapseHeaderItems.classList.remove("max-md:tw-opacity-0")
+        collapseHeaderItems.classList.add("opacity-100",)
+        collapseHeaderItems.style.width = "60vw"
+        collapseBtn.classList.remove("bi-list")
+        collapseBtn.classList.add("bi-x", "max-lg:tw-fixed")
+        isHeaderCollapsed = false
 
+        setTimeout(() => window.addEventListener("click", onHeaderClickOutside), 1)
+
+    } else {
+        collapseHeaderItems.classList.remove("opacity-100")
+        collapseHeaderItems.style.width = "0vw"
+        collapseBtn.classList.remove("bi-x", "max-lg:tw-fixed")
+        collapseBtn.classList.add("bi-list")
+        isHeaderCollapsed = true
+        window.removeEventListener("click", onHeaderClickOutside)
+
+    }
+}
 
 function responsive() {
     if (window.innerWidth > RESPONSIVE_WIDTH) {
